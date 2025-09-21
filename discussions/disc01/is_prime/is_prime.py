749af6136b3d71c90b129eb1952f52d98f651af4
Max-dot668 <maxiyouit@gmail.com>
2025-09-21 13:55:17 -0700

    Complete disc01 assignments

---
+++ discussions/disc01/is_prime/is_prime.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return False
+    i = 2
+    while i <= int( sqrt(n) ):
+        if n % i == 0:
+            return False
+        i = i + 1
+        return True
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ discussions/disc01/unique_digits/unique_digits.py
@@ -0,0 +1,33 @@
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(101) # 0 and 1
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    count = 0
+    for k in range(10):
+        if has_digit(n, k):
+            count += 1
+    return count
+
+
+def has_digit(n, k):
+    """Returns whether k is a digit in n.
+
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    assert k >= 0 and k < 10
+    "*** YOUR CODE HERE ***"
+    while n > 0:
+        if n % 10 == k:
+            return True
+        n //= 10
+    return False
